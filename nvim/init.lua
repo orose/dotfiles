dbd1df3d46f3b92f95b63c494232e577dca84285
Øystein Rose <oystein.rose@gmail.com>
2022-10-29 22:40:55 +0200

    Initial nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,22 @@
-vim.cmd([[
-  set runtimepath^=~/.vim runtimepath+=~/.vim/after
-  let &packpath = &runtimepath
-  source ~/.vimrc
-]])
-
-require'lspconfig'.tsserver.setup {}
+require "user.impatient"
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.autocommands"
+require "user.colorscheme"
+require "user.cmp"
+require "user.telescope"
+require "user.gitsigns"
+require "user.treesitter"
+require "user.autopairs"
+require "user.comment"
+require "user.nvim-tree"
+require "user.bufferline"
+require "user.lualine"
+require "user.toggleterm"
+require "user.project"
+require "user.illuminate"
+require "user.indentline"
+require "user.alpha"
+require "user.lsp"
+require "user.dap"
