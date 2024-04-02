c353351f3a28687715e0a230ceb0c158bcd7ae66
Øystein Rose <oystein.rose@gmail.com>
2024-04-02 23:45:54 +0200

    Installed harpoon plugin

---
+++ nvim_kickstart/lua/custom/plugins/harpoon.lua
@@ -0,0 +1,23 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup()
+
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end, { desc = "Harpoon: Add file to list" })
+		vim.keymap.set("n", "<leader>l", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "Harpoon: Show list" })
+		vim.keymap.set("n", "<C-h>", function()
+			harpoon:list():prev()
+		end, { desc = "Harpoon: Goto previous file" })
+		vim.keymap.set("n", "<C-l>", function()
+			harpoon:list():next()
+		end, { desc = "Harpoon: Goto next file" })
+	end,
+}
+-- vim: ts=2 sts=2 sw=2 et
