9682252a1c6cc46dd881c22118008c33aad57d55
Øystein Rose <oystein.rose@gmail.com>
2024-09-09 12:14:08 +0200

    tsserver => ts_ls.

--- nvim_kickstart/init.lua
+++ nvim_kickstart/init.lua
@@ -529,7 +529,7 @@ require("lazy").setup({
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`tsserver`) will work just fine
-				tsserver = {},
+				ts_ls = {},
 				--
 
 				rust_analyzer = {
