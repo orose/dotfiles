138cd9cfa317bdcbf607ca012125eec0afcc04d6
Øystein Rose <oystein.rose@gmail.com>
2024-11-29 22:52:04 +0100

    Formatting html and javascript

--- nvim/init.lua
+++ nvim/init.lua
@@ -250,11 +250,12 @@ require("lazy").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 				json = { "jq" },
+				html = { "prettierd", "prettier", stop_after_first = true },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
-				-- javascript = { "prettierd", "prettier", stop_after_first = true },
 			},
 		},
 	},
