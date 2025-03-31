2f1025ae1bd2c5438493836c286e37478885b115
Øystein Rose <Oystein.Rose@skatteetaten.no>
2025-03-31 11:15:24 +0200

    Added transparent background

--- nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -1,22 +1,20 @@
 return { -- You can easily change to a different colorscheme.
-	-- Change the name of the colorscheme plugin below, and then
-	-- change the command in the config to whatever the name of that colorscheme is.
-	--
-	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 	-- "folke/tokyonight.nvim",
 	-- "catppuccin/nvim",
 	"rose-pine/neovim",
 	name = "rose-pine",
 	priority = 1000, -- Make sure to load this before all the other start plugins.
 	init = function()
-		-- Load the colorscheme here.
-		-- Like many other themes, this one has different styles, and you could load
-		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-		-- vim.cmd.colorscheme("tokyonight-night")
+		require("rose-pine").setup({
+			variant = "dawn",
+			disable_background = true,
+		})
+
 		vim.cmd.colorscheme("rose-pine-dawn")
 
-		-- You can configure highlights by doing something like:
-		-- vim.cmd.hi("Comment gui=none")
+		-- Ekstra konfigurasjoner for å sikre transparens
+		vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
 	end,
 }
 
