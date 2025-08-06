b7a23de932948a3b6b7e38a8790dab153f101925
Øystein Rose <oystein.rose@gmail.com>
2025-08-06 23:29:22 +0200

    Some telescope tweaks. Better pickers.

--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -23,50 +23,24 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 	},
 	config = function()
-		-- Telescope is a fuzzy finder that comes with a lot of different things that
-		-- it can fuzzy find! It's more than just a "file finder", it can search
-		-- many different aspects of Neovim, your workspace, LSP, and more!
-		--
-		-- The easiest way to use Telescope, is to start by doing something like:
-		--  :Telescope help_tags
-		--
-		-- After running this command, a window will open up and you're able to
-		-- type in the prompt window. You'll see a list of `help_tags` options and
-		-- a corresponding preview of the help.
-		--
-		-- Two important keymaps to use while in Telescope are:
-		--  - Insert mode: <c-/>
-		--  - Normal mode: ?
-		--
-		-- This opens a window that shows you all of the keymaps for the current
-		-- Telescope picker. This is really useful to discover what Telescope can
-		-- do as well as how to actually do it!
-
-		-- [[ Configure Telescope ]]
-		-- See `:help telescope` and `:help telescope.setup()`
 		require("telescope").setup({
-			-- You can put your default mappings / updates / etc. in here
-			--  All the info you're looking for is in `:help telescope.setup()`
-			--
-			-- defaults = {
-			--   mappings = {
-			--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-			--   },
-			-- },
 			defaults = {
 				layout_strategy = "vertical",
 				layout_config = { height = 0.75 },
 			},
-			-- pickers = {
-			-- 	find_files = {
-			-- 		theme = "dropdown",
-			-- 		previewer = false,
-			-- 	},
-			-- 	live_grep = {
-			-- 		theme = "dropdown",
-			-- 		previewer = true,
-			-- 	},
-			-- },
+			pickers = {
+				lsp_references = { show_line = false },
+				live_grep = {
+					layout_strategy = "vertical",
+					layout_config = {
+						width = 0.8,
+						height = 0.8,
+						preview_cutoff = 10, -- viser preview selv ved små vinduer
+						prompt_position = "bottom", -- (kan være "top" eller "bottom")
+					},
+					previewer = true,
+				},
+			},
 			extensions = {
 				["ui-select"] = {
 					require("telescope.themes").get_dropdown(),
