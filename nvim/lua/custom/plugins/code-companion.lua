d1e12625c3bd630186168dd909602825b128abb8
Øystein Rose <oystein.rose@gmail.com>
2025-09-10 19:36:39 +0200

    Fixed code companion config.

--- nvim/lua/custom/plugins/code-companion.lua
+++ nvim/lua/custom/plugins/code-companion.lua
@@ -14,30 +14,29 @@ return {
 			},
 		},
 		adapters = {
-			anthropic = function()
-				return require("codecompanion.adapters").extend("anthropic", {
-					env = {
-						api_key = "ANTHROPIC_API_KEY",
-					},
-					schema = {
-						model = {
-							default = "claude-sonnet-4-20250514",
+			http = {
+				anthropic = function()
+					return require("codecompanion.adapters").extend("anthropic", {
+						env = {
+							api_key = "ANTHROPIC_API_KEY",
 						},
-					},
-				})
-			end,
-			copilot = function()
-				return require("codecompanion.adapters").extend("copilot", {})
-			end,
+						schema = {
+							model = {
+								default = "claude-sonnet-4-20250514",
+							},
+						},
+					})
+				end,
+				copilot = function()
+					return require("codecompanion.adapters").extend("copilot", {})
+				end,
+			},
 		},
 	},
-
 	config = function(_, opts)
 		require("codecompanion").setup(opts)
-
 		vim.keymap.set("n", "<Leader>aa", "<cmd>CodeCompanionActions<CR>", { desc = "CodeCompanion Actions" })
 		vim.keymap.set("v", "<Leader>aa", "<cmd>CodeCompanionActions<CR>", { desc = "CodeCompanion Actions" })
 	end,
 }
-
 -- vim: ts=2 sts=2 sw=2 et
