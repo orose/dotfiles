752ec3cadf33ac296c267e77bdc8dec0c3c74150
Øystein Rose <Oystein.Rose@skatteetaten.no>
2025-12-08 10:37:19 +0100

    Removed pomo-plugin and other minor tweaks and bugfixes

--- nvim/lua/custom/plugins/obsidian.lua
+++ nvim/lua/custom/plugins/obsidian.lua
@@ -6,6 +6,7 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		require("obsidian").setup({
+			ui = { enable = false },
 			workspaces = { {
 				name = "Notater",
 				path = "~/Notater",
--- nvim/lua/custom/plugins/render-markdown.lua
+++ nvim/lua/custom/plugins/render-markdown.lua
@@ -1,15 +1,19 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+	-- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
-	opts = { heading = {
-		border = true,
-		right_pad = 1,
-		width = "block",
-	} },
+	opts = {
+		latex = { enabled = false },
+		yaml = { enabled = false },
+		heading = {
+			border = true,
+			right_pad = 1,
+			width = "block",
+		},
+	},
 }
 
 -- vim: ts=2 sts=2 sw=2 et
